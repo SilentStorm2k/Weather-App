354fd7f4c799d61c1a41eaed5e2a8378709e98c1
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-24 20:19:38 +0530

    updated testing

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,25 @@
 import '../css/styles.css';
 import '../css/reset.css';
+import { getWeatherGivenCity, getWeatherGivenLocation } from './weatherApi';
 
 console.log('hello world');
+
+// getWeatherGivenCity('chennai')
+//     .then((weather) => console.log(weather))
+//     .catch((err) => console.log(err));
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (pos) =>
+                getWeatherGivenLocation(pos)
+                    .then((weather) => console.log(weather))
+                    .catch((err) => console.log(err)),
+            (err) => console.log(err),
+        );
+    } else {
+        console.log('Geolocation not supported in your browser');
+    }
+}
+
+// getLocation();
