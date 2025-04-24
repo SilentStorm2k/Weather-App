840cd82c4d0f579cd26ba13e3000020091ce68d6
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-24 13:00:37 +0530

    added dotenv extension for handling API key

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/ts/index.ts',
@@ -31,6 +32,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/template.html',
         }),
+        new Dotenv(),
     ],
     output: {
         filename: 'bundle.js',
