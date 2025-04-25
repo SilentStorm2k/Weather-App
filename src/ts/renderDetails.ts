77c773e999d2f3f94393ceca7788cdafd58eaf58
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-26 03:16:15 +0530

    [feat] added current conditions div elements

--- src/ts/renderDetails.ts
+++ src/ts/renderDetails.ts
@@ -1,8 +1,59 @@
 import {
     getWeatherGivenCity,
     getWeatherGivenLocation,
+    tempWeatherReport,
     WeatherReport,
 } from './weatherApi';
+import '../css/weather.styles.css';
+import clearday from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/clear-day.png';
+import clearnight from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/clear-night.png';
+import cloudy from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/cloudy.png';
+import fog from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/fog.png';
+import hail from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/hail.png';
+import partlycloudyday from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/partly-cloudy-day.png';
+import partlycloudynight from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/partly-cloudy-night.png';
+import rainsnowshowersday from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/rain-snow-showers-day.png';
+import rainsnowshowersnight from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/rain-snow-showers-night.png';
+import rainsnow from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/rain-snow.png';
+import rain from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/rain.png';
+import showersday from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/showers-day.png';
+import showersnight from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/showers-night.png';
+import sleet from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/sleet.png';
+import snowshowersday from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/snow-showers-day.png';
+import snowshowersnight from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/snow-showers-night.png';
+import snow from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/snow.png';
+import thunerrain from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/thunder-rain.png';
+import thundershowersday from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/thunder-showers-day.png';
+import thundershowersnight from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/thunder-showers-night.png';
+import thunder from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/thunder.png';
+import wind from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/wind.png';
+
+const weatherContainer = document.querySelector('.weatherReport');
+
+const weatherIcons = {
+    'clear-day': clearday,
+    'clear-night': clearnight,
+    cloudy: cloudy,
+    fog: fog,
+    hail: hail,
+    'partly-cloudy-day': partlycloudyday,
+    'partly-cloudy-night': partlycloudynight,
+    'rain-snow-showers-day': rainsnowshowersday,
+    'rain-snow-showers-night': rainsnowshowersnight,
+    'rain-snow': rainsnow,
+    rain: rain,
+    'showers-day': showersday,
+    'showers-night': showersnight,
+    sleet: sleet,
+    'snow-showers-day': snowshowersday,
+    'snow-showers-night': snowshowersnight,
+    snow: snow,
+    'thunder-rain': thunerrain,
+    'thunder-showers-day': thundershowersday,
+    'thunder-showers-night': thundershowersnight,
+    thunder: thunder,
+    wind: wind,
+};
 
 /**
  * @param unit: passing metric/US unit
@@ -17,17 +68,20 @@ export async function render(
 ) {
     let weatherDetails: WeatherReport | undefined;
     try {
-        if (geolocation)
-            weatherDetails = await getWeatherGivenLocation(geolocation, unit);
-        else if (city) weatherDetails = await getWeatherGivenCity(city, unit);
-        else throw new Error('Must provide city or provide location');
+        // if (geolocation)
+        //     weatherDetails = await getWeatherGivenLocation(geolocation, unit);
+        // else if (city) weatherDetails = await getWeatherGivenCity(city, unit);
+        // else throw new Error('Must provide city or provide location');
+        weatherDetails = tempWeatherReport();
+        console.log('WORksS', weatherDetails);
 
         if (weatherDetails) {
-            renderCurrentContidions(weatherDetails);
-            renderFeelsLike(weatherDetails);
+            const unitString = unit == 'us' ? '°F' : '°C';
+            renderCurrentConditions(weatherDetails, unitString);
+            renderFeelsLike(weatherDetails, unitString);
             renderHumidity(weatherDetails);
             renderDescription(weatherDetails);
-            renderFutureForecast(weatherDetails);
+            renderFutureForecast(weatherDetails, unitString);
         } else {
             console.log(
                 'Weather report result is undefined, rendering skipped',
@@ -41,22 +95,46 @@ export async function render(
         throw error;
     }
 }
-function renderCurrentContidions(weatherDetails: {}) {
+function renderCurrentConditions(weatherDetails: WeatherReport, unit: string) {
+    const currentConditionDiv =
+        weatherContainer?.querySelector('.currentConditions');
+    const locationDiv = currentConditionDiv?.children[0] as HTMLDivElement;
+    const tempContainerDiv = currentConditionDiv?.children[1] as HTMLDivElement;
+    const shortDescriptionDiv = currentConditionDiv
+        ?.children[2] as HTMLDivElement;
+
+    locationDiv.innerText = weatherDetails.location;
+    shortDescriptionDiv.innerText = weatherDetails.conditions;
+
+    const icon = tempContainerDiv.children[0] as HTMLImageElement;
+    const currentTempDiv = tempContainerDiv.children[1] as HTMLDivElement;
+    const minMaxTempDiv = tempContainerDiv.children[2] as HTMLDivElement;
+
+    const iconKey = weatherDetails.weatherIcon;
+    icon.src =
+        weatherIcons[iconKey as keyof typeof weatherIcons] ||
+        weatherIcons['clear-day'];
+    currentTempDiv.innerText = weatherDetails.temperature.toString() + unit;
+
+    const maxTempDiv = minMaxTempDiv.children[0] as HTMLDivElement;
+    const minTempDiv = minMaxTempDiv.children[1] as HTMLDivElement;
+
+    maxTempDiv.innerText = weatherDetails.tempMax.toString() + unit;
+    minTempDiv.innerText = weatherDetails.tempMin.toString() + unit;
+}
+
+function renderFeelsLike(weatherDetails: WeatherReport, unit: string) {
     throw new Error('Function not implemented.');
 }
 
-function renderFeelsLike(weatherDetails: {}) {
+function renderHumidity(weatherDetails: WeatherReport) {
     throw new Error('Function not implemented.');
 }
 
-function renderHumidity(weatherDetails: {}) {
+function renderDescription(weatherDetails: WeatherReport) {
     throw new Error('Function not implemented.');
 }
 
-function renderDescription(weatherDetails: {}) {
-    throw new Error('Function not implemented.');
-}
-
-function renderFutureForecast(weatherDetails: {}) {
+function renderFutureForecast(weatherDetails: WeatherReport, unit: string) {
     throw new Error('Function not implemented.');
 }
