87568d685859a114573b4e8bfccbfe71137b6c4d
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-27 02:50:02 +0530

    [refactor] moved around so mode readable

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,31 +1,16 @@
 import '../css/styles.css';
 import '../css/reset.css';
-import {
-    getWeatherGivenCity,
-    getWeatherGivenLocation,
-    tempWeatherReport,
-} from './weatherApi';
 import { render } from './renderDetails';
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (pos) =>
-                getWeatherGivenLocation(pos)
-                    .then((weather) => console.log(weather))
-                    .catch((err) => console.log(err)),
-            (err) => console.log(err),
-        );
-    } else {
-        console.log('Geolocation not supported in your browser');
-    }
-}
 
 let cityValue = 'New York';
 let unit = 'metric';
 let geoLocation: GeolocationPosition | undefined = undefined;
-
+const loadingIcon = document.querySelector('.loading');
+const searchIcon = document.querySelector('.search');
 const input = document.querySelector('input');
+const unitToggle = document.getElementById('unitToggle') as HTMLInputElement;
+const useLocation = document.getElementById('location') as HTMLButtonElement;
+
 input?.addEventListener('keypress', (e) => {
     if (e.key == 'Enter') {
         e.preventDefault();
@@ -33,41 +18,53 @@ input?.addEventListener('keypress', (e) => {
     }
 });
 
-const search = document.querySelector('svg');
-search?.addEventListener('click', (e) => {
+searchIcon?.addEventListener('click', (e) => {
     e.preventDefault();
     handleSubmit(e);
 });
 
-function handleSubmit(ev: Event) {
-    if (input) {
-        cityValue = input.value == '' ? cityValue : input.value;
-        render(unit, cityValue);
-    }
-}
-
-window.onload = () => {
-    // render('metric', undefined, undefined, tempWeatherReport());
-    render(unit, cityValue);
-};
-
-const unitToggle = document.getElementById('unitToggle') as HTMLInputElement;
-
 unitToggle.addEventListener('change', () => {
     if (unitToggle.checked) unit = 'metric';
     else unit = 'us';
-    render(unit, cityValue);
+    handleDefaultRender(unit, cityValue);
 });
-
-const useLocation = document.getElementById('location') as HTMLButtonElement;
 
 useLocation.addEventListener('click', (e) => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
             geoLocation = position;
-            render(unit, undefined, position);
+            handleDefaultRender(unit, undefined, geoLocation);
         });
     } else {
         console.error('Location permission not available');
     }
 });
+
+function handleSubmit(ev: Event) {
+    if (input) {
+        cityValue = input.value == '' ? cityValue : input.value;
+        handleDefaultRender(unit, cityValue);
+    }
+}
+
+async function handleDefaultRender(
+    unit: string,
+    cityValue?: string,
+    geoLocation?: GeolocationPosition,
+) {
+    loadingIcon?.classList.remove('hidden');
+    searchIcon?.classList.add('hidden');
+    try {
+        await render(unit, cityValue, geoLocation);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        loadingIcon?.classList.add('hidden');
+        searchIcon?.classList.remove('hidden');
+    }
+}
+
+window.onload = () => {
+    // render('metric', undefined, undefined, tempWeatherReport());
+    handleDefaultRender(unit, cityValue);
+};
