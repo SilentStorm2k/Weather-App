faafbd87305c2eed750b15b6b4bbfb9b190b318d
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-29 19:39:18 +0530

    [feat] updated assets and rewrote many render functions  to clean then modify the element

--- src/ts/renderDetails.ts
+++ src/ts/renderDetails.ts
@@ -29,9 +29,9 @@ import thunder from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Co
 import wind from '../assets/visualcrossing WeatherIcons main PNG-3rd Set - Color/wind.png';
 import humidity from '../assets/humidity.png';
 import info from '../assets/information.png';
+import low from '../assets/low.png';
+import high from '../assets/high.png';
 import { getDay, isToday, isTomorrow } from 'date-fns';
-
-const weatherContainer = document.querySelector('.weatherReport');
 
 const dayMap = {
     0: 'SUN',
@@ -90,9 +90,11 @@ export async function render(
 
         if (weatherDetails) {
             const unitString = unit == 'us' ? '°F' : '°C';
+            renderCurrentLocation(weatherDetails);
             renderCurrentConditions(weatherDetails, unitString);
-            renderFeelsLike(weatherDetails, unitString);
+            // renderFeelsLike(weatherDetails, unitString);
             renderHumidity(weatherDetails);
+            renderMinMaxTemp(weatherDetails, unitString);
             renderDescription(weatherDetails);
             renderFutureForecast(weatherDetails, unitString);
         } else {
@@ -116,32 +118,81 @@ export async function render(
  * @param unit:         The unit of temperature ('°F' or '°C').
  * @throws Error if the weatherContainer or its children are not found.
  */
+// function renderCurrentConditions(weatherDetails: WeatherReport, unit: string) {
+//     const currentConditionDiv = document?.querySelector('.currentConditions');
+//     const locationDiv = currentConditionDiv?.children[0] as HTMLDivElement;
+//     const tempContainerDiv = currentConditionDiv?.children[1] as HTMLDivElement;
+//     const shortDescriptionDiv = currentConditionDiv
+//         ?.children[2] as HTMLDivElement;
+
+//     locationDiv.innerText = weatherDetails.location;
+//     shortDescriptionDiv.innerText = weatherDetails.conditions;
+
+//     const icon = tempContainerDiv.children[0] as HTMLImageElement;
+//     const currentTempDiv = tempContainerDiv.children[1] as HTMLDivElement;
+//     const minMaxTempDiv = tempContainerDiv.children[2] as HTMLDivElement;
+
+//     const iconKey = weatherDetails.weatherIcon;
+//     icon.src =
+//         weatherIcons[iconKey as keyof typeof weatherIcons] ||
+//         weatherIcons['clear-day'];
+//     icon.alt = iconKey.split('-').join(' ');
+//     currentTempDiv.innerText = weatherDetails.temperature.toString() + unit;
+
+//     const maxTempDiv = minMaxTempDiv.children[0] as HTMLDivElement;
+//     const minTempDiv = minMaxTempDiv.children[1] as HTMLDivElement;
+
+//     maxTempDiv.innerText = weatherDetails.tempMax.toString() + unit;
+//     minTempDiv.innerText = weatherDetails.tempMin.toString() + unit;
+// }
+
 function renderCurrentConditions(weatherDetails: WeatherReport, unit: string) {
-    const currentConditionDiv =
-        weatherContainer?.querySelector('.currentConditions');
-    const locationDiv = currentConditionDiv?.children[0] as HTMLDivElement;
-    const tempContainerDiv = currentConditionDiv?.children[1] as HTMLDivElement;
-    const shortDescriptionDiv = currentConditionDiv
-        ?.children[2] as HTMLDivElement;
+    const currentConditionDiv = document?.querySelector(
+        '.currentConditions',
+    ) as HTMLDivElement;
+    cleanDiv(currentConditionDiv);
 
-    locationDiv.innerText = weatherDetails.location;
-    shortDescriptionDiv.innerText = weatherDetails.conditions;
-
-    const icon = tempContainerDiv.children[0] as HTMLImageElement;
-    const currentTempDiv = tempContainerDiv.children[1] as HTMLDivElement;
-    const minMaxTempDiv = tempContainerDiv.children[2] as HTMLDivElement;
-
+    // const tempContainerDiv = document.createElement('div');
+    const icon = document.createElement('img');
+    const currentTemp = document.createElement('div');
     const iconKey = weatherDetails.weatherIcon;
     icon.src =
         weatherIcons[iconKey as keyof typeof weatherIcons] ||
         weatherIcons['clear-day'];
-    currentTempDiv.innerText = weatherDetails.temperature.toString() + unit;
+    icon.alt = iconKey.split('-').join(' ');
+    currentTemp.innerText = weatherDetails.temperature.toString() + unit;
+    currentConditionDiv?.appendChild(icon);
+    currentConditionDiv?.appendChild(currentTemp);
+    // currentConditionDiv?.appendChild(tempContainerDiv);
+}
 
-    const maxTempDiv = minMaxTempDiv.children[0] as HTMLDivElement;
-    const minTempDiv = minMaxTempDiv.children[1] as HTMLDivElement;
+function renderMinMaxTemp(weatherDetails: WeatherReport, unit: string) {
+    const max = document.querySelector('.maxTemp') as HTMLDivElement;
+    const min = document.querySelector('.minTemp') as HTMLDivElement;
+    cleanDiv(max);
+    cleanDiv(min);
+
+    const maxIcon = document.createElement('img');
+    const maxTempDiv = document.createElement('div');
+    const minIcon = document.createElement('img');
+    const minTempDiv = document.createElement('div');
 
     maxTempDiv.innerText = weatherDetails.tempMax.toString() + unit;
     minTempDiv.innerText = weatherDetails.tempMin.toString() + unit;
+    maxIcon.src = high;
+    minIcon.src = low;
+
+    max.appendChild(maxIcon);
+    max.appendChild(maxTempDiv);
+    min.appendChild(minIcon);
+    min.appendChild(minTempDiv);
+}
+
+function renderCurrentLocation(weatherDetails: WeatherReport) {
+    const locationDiv = document.querySelector(
+        '.currentLocation',
+    ) as HTMLDivElement;
+    locationDiv.innerText = weatherDetails.location;
 }
 
 /**
@@ -152,7 +203,7 @@ function renderCurrentConditions(weatherDetails: WeatherReport, unit: string) {
  * @throws Error if the weatherContainer or feelsLike children are not found.
  */
 function renderFeelsLike(weatherDetails: WeatherReport, unit: string) {
-    const feelsLikeDiv = weatherContainer?.querySelector('.feelsLike');
+    const feelsLikeDiv = document?.querySelector('.feelsLike');
     const mainText = feelsLikeDiv?.children[0] as HTMLDivElement;
     const temp = feelsLikeDiv?.children[1] as HTMLDivElement;
 
@@ -167,17 +218,16 @@ function renderFeelsLike(weatherDetails: WeatherReport, unit: string) {
  * @throws Error if the weatherContainer or humidity children are not found.
  */
 function renderHumidity(weatherDetails: WeatherReport) {
-    const humidityDiv = weatherContainer?.querySelector('.humidity');
-    const mainText = humidityDiv?.children[0] as HTMLDivElement;
-    const humidityContainer = humidityDiv?.children[1] as HTMLDivElement;
+    const humidityDiv = document?.querySelector('.humidity') as HTMLDivElement;
+    cleanDiv(humidityDiv);
 
-    mainText.innerText = 'Humidity';
-
-    const humidityIcon = humidityContainer.children[0] as HTMLImageElement;
-    const humidityValue = humidityContainer.children[1] as HTMLDivElement;
-
+    const humidityIcon = document.createElement('img');
+    const humidityValue = document.createElement('div');
     humidityIcon.src = humidity;
     humidityValue.innerText = weatherDetails.humidity + '%';
+
+    humidityDiv?.appendChild(humidityIcon);
+    humidityDiv?.appendChild(humidityValue);
 }
 
 /**
@@ -187,12 +237,17 @@ function renderHumidity(weatherDetails: WeatherReport) {
  * @throws Error if the weatherContainer or description children are not found.
  */
 function renderDescription(weatherDetails: WeatherReport) {
-    const descriptionDiv = weatherContainer?.querySelector('.description');
-    const descriptionIcon = descriptionDiv?.children[0] as HTMLImageElement;
-    const mainText = descriptionDiv?.children[1] as HTMLDivElement;
+    const descriptionDiv = document.querySelector(
+        '.description',
+    ) as HTMLDivElement;
+    cleanDiv(descriptionDiv);
+    const descriptionIcon = document.createElement('img');
+    const mainText = document.createElement('div');
 
     descriptionIcon.src = info;
     mainText.innerText = weatherDetails.description;
+    descriptionDiv?.appendChild(descriptionIcon);
+    descriptionDiv?.appendChild(mainText);
 }
 
 /**
@@ -203,7 +258,7 @@ function renderDescription(weatherDetails: WeatherReport) {
  * @throws Error if the weatherContainer or futureForecast container is not found.
  */
 function renderFutureForecast(weatherDetails: WeatherReport, unit: string) {
-    const futureForecastDiv = weatherContainer?.querySelector(
+    const futureForecastDiv = document?.querySelector(
         '.futureForecast',
     ) as HTMLDivElement;
     cleanDiv(futureForecastDiv);
